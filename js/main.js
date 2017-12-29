4db32d39add5f5e4b164eeea334e012f1a548149
dkkim6200 <dkkim6200@gmail.com>
2017-12-29 15:15:43 -0800

    Added demo videos for each project.

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ var projects = [{
 		"title": "Nano Balls",
 		"folder": "nano_balls",
 		"skills": ["Unity3D", "C#"],
-		"description": "",
+		"description": '<iframe width="560" height="315" src="https://www.youtube.com/embed/E7-Xl_8F__A" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>',
 		"thumbnail": "nano_balls_1.png",
 		"images": ["nano_balls_1.png", "nano_balls_2.png", "nano_balls_3.png", "nano_balls_4.png", "nano_balls_5.png", "nano_balls_6.png"]
 	},
@@ -21,7 +21,7 @@ var projects = [{
 		"title": "Reactor Engine",
 		"folder": "reactor_engine",
 		"skills": ["C++", "OpenGL", "Blender"],
-		"description": "",
+		"description": '<iframe width="560" height="315" src="https://www.youtube.com/embed/7NeS1_4fmm0" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe><br><br>Reactor Engine!',
 		"thumbnail": "reactor_engine_1.png",
 		"images": ["reactor_engine_1.png", "reactor_engine_2.png", "reactor_engine_3.png"]
 	},
@@ -30,7 +30,7 @@ var projects = [{
 		"title": "Spinning Coin",
 		"folder": "spinning_coin",
 		"skills": ["Unity3D", "C#", "Blender"],
-		"description": "",
+		"description": '<iframe width="560" height="315" src="https://www.youtube.com/embed/amC7tpIgLnA" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe><br><br>',
 		"thumbnail": "spinning_coin_1.png",
 		"images": ["spinning_coin_1.png", "spinning_coin_2.png"]
 	},
@@ -48,7 +48,7 @@ var projects = [{
 		"title": "Tic Tac Toe",
 		"folder": "tic_tac_toe",
 		"skills": ["C++"],
-		"description": "",
+		"description": '<iframe width="560" height="315" src="https://www.youtube.com/embed/fZdl4v0SNh0" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe><br><br>Tic-Tac-Toe with AI implemented',
 		"thumbnail": "tic_tac_toe_1.png",
 		"images": ["tic_tac_toe_1.png"]
 	},
@@ -57,7 +57,7 @@ var projects = [{
 		"title": "Vector World",
 		"folder": "vector_world",
 		"skills": ["Java", "C++", "OpenGL"],
-		"description": "",
+		"description": '<iframe width="560" height="315" src="https://www.youtube.com/embed/DhzuMXQSKn8" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe><br><br>VectorWorld!',
 		"thumbnail": "vector_world_1.png",
 		"images": ["vector_world_1.png", "vector_world_2.png"]
 	},
@@ -138,6 +138,7 @@ $(document).ready(function() {
 		for (var i = 0; i < curProject.skills.length; i++) {
 			toAppend += curProject.skills[i] + ", ";
 		}
+		toAppend = toAppend.substring(0, toAppend.length-2);
 		$('.project-skills').append(toAppend);
 
 		// Description
