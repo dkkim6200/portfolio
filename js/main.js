317ac432dd33e1d32e1e0169ade031f544e1a227
Daekun Kim <dkkim6200@gmail.com>
2019-07-11 22:58:36 +0900

    Seperated project descriptions to seperate HTML files

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var projects = [{
 		"title": "Kindlers Network",
 		"folder": "kindlers",
 		"skills": ["PHP", "Bootstrap", "MySQL", "HTML", "CSS", "JS", "jQuery", "AJAX"],
-		"description": '<a href="https://github.com/dkkim6200/Kindlers">View on GitHub</a><br><br>Created a web application to register new members, sign up online for volunteer opportunities, and display well-formatted list of volunteer events and hours of each member for my school\'s Kindler\'s Society (NGO for donating English books to the third-world children).<br><br>Login and sign-up system was implemented with PHP and MySQL, and AJAX to update the volunteer event status.<br><br>Other functionality include executive-only volunteer event posting, as well as managing volunteer hours of the general members.',
+		"description": 'kindlers.html',
 		"thumbnail": "kindlers_1.jpg",
 		"images": ["kindlers_1.jpg", "kindlers_2.jpg", "kindlers_3.jpg", "kindlers_4.jpg"]
 	},
@@ -12,7 +12,7 @@ var projects = [{
 		"title": "Nano Balls",
 		"folder": "nano_balls",
 		"skills": ["Unity", "C#"],
-		"description": '<a href="http://goo.gl/hC1VKn">View on Google Play Store</a><br><br>Developed a rolling ball puzzle game using Unity. It has 20 levels, and divided into 2 themes (sky and ground).<br><br>This game was inspired from Unity\'s "Roll-a-ball tutorial" (<a href="https://unity3d.com/learn/tutorials/s/roll-ball-tutorial">https://unity3d.com/learn/tutorials/s/roll-ball-tutorial</a>). It is currently being sold on the Google Play Store. Nano Balls is not on Apple App Store anymore.<br><br><iframe width="560" height="315" src="https://www.youtube.com/embed/E7-Xl_8F__A" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe><br><br>',
+		"description": 'nano_balls.html',
 		"thumbnail": "nano_balls_1.jpg",
 		"images": ["nano_balls_1.jpg", "nano_balls_2.jpg", "nano_balls_3.jpg", "nano_balls_4.jpg", "nano_balls_5.jpg", "nano_balls_6.jpg"]
 	},
@@ -21,7 +21,7 @@ var projects = [{
 		"title": "Reactor Engine",
 		"folder": "reactor_engine",
 		"skills": ["C++", "OpenGL", "Blender"],
-		"description": '<a href="https://github.com/dkkim6200/ReactorEngine">View on GitHub</a><br><br>Created Unity-styled game engine using only C++ and OpenGL. It implemented <a href="https://en.wikipedia.org/wiki/Entity%E2%80%93component%E2%80%93system">entity-component-system</a> to structure the game objects and their interactions. It was built with multi-platform implementation in mind, thus the system-specific part of the engine (accessing file, user input, etc.) has been carefully seperated from other parts.<br><br>While using other game engines like Unity and Unreal, I wanted to have a thorough understanding of their inner-workings, so I decided to make a game engine of my own. It is one of the most challenging project I have taken on, yet it was very statisfying to work with.<br><br><iframe width="560" height="315" src="https://www.youtube.com/embed/7NeS1_4fmm0" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe><br><br>Reactor Engine!',
+		"description": 'reactor_engine.html',
 		"thumbnail": "reactor_engine_1.jpg",
 		"images": ["reactor_engine_1.jpg", "reactor_engine_2.jpg", "reactor_engine_3.jpg"]
 	},
@@ -30,7 +30,7 @@ var projects = [{
 		"title": "Spinning Coin",
 		"folder": "spinning_coin",
 		"skills": ["Unity", "C#", "Blender"],
-		"description": '<a href="http://goo.gl/JNSRfE">View on Google Play Store</a><br><br>Inspired from the game "Flappy Bird", I developed a simple game where the objective is to balance the ball to go past as many obstacles as possible.<br><br>It implements Facebook-sharing feature, in which it automatically takes a screenshot of the result and posts it on user\'s Facebook with a custom message.<br><br><iframe width="560" height="315" src="https://www.youtube.com/embed/amC7tpIgLnA" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe><br><br>',
+		"description": 'spinning_coin.html',
 		"thumbnail": "spinning_coin_1.jpg",
 		"images": ["spinning_coin_1.jpg", "spinning_coin_2.jpg"]
 	},
@@ -39,7 +39,7 @@ var projects = [{
 		"title": "Sympo",
 		"folder": "sympo",
 		"skills": ["Objective-C", "Swift 2", "Apache Tomcat", "Java Servlet", "MySQL"],
-		"description": "A social network application for iOS using Swift 2 for UI and Objective-C for app logic. Focuses on sharing events and casual encouragement for others to attent the event.<br><br>The login and event feed server was implement using Apache Tomcat and Java Servlet. A relational MySQL database was used, in which simple hashing algorithm to store user's password information.",
+		"description": "sympo.html",
 		"thumbnail": "sympo_1.jpg",
 		"images": ["sympo_1.jpg"]
 	},
@@ -48,7 +48,7 @@ var projects = [{
 		"title": "Tic Tac Toe",
 		"folder": "tic_tac_toe",
 		"skills": ["C++"],
-		"description": 'One of my projects from Info. Tech. 10 class. I was to create a tic-tac-toe (OXO) game with a simple AI algorithm built by myself. The AI can predict up to 3 turns out, thus making it almost unbeatable.<br><br><iframe width="560" height="315" src="https://www.youtube.com/embed/fZdl4v0SNh0" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe><br><br>',
+		"description": 'tic_tac_toe.html',
 		"thumbnail": "tic_tac_toe_1.jpg",
 		"images": ["tic_tac_toe_1.jpg"]
 	},
@@ -57,7 +57,7 @@ var projects = [{
 		"title": "Vector World",
 		"folder": "vector_world",
 		"skills": ["Java", "C++"],
-		"description": 'Created a simple 3D rendering program from scratch using only C++ and no graphics library. A predecessor of the Reactor Engine project.<br><br>I wanted to get deeper understanding of what kind of mathematics is used behind 3D rendering techniques, and decided to build a rendering engine of my own. At the end, by expanding this project into a game engine (Reactor Engine), I was able to grasp a thorough understanding of 3D graphics.<br><br><iframe width="560" height="315" src="https://www.youtube.com/embed/DhzuMXQSKn8" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe><br><br>VectorWorld!',
+		"description": 'vector_world.html',
 		"thumbnail": "vector_world_1.jpg",
 		"images": ["vector_world_1.jpg", "vector_world_2.jpg"]
 	},
@@ -66,7 +66,7 @@ var projects = [{
 		"title": "HeadCheck Health<br>Automation & Testing",
 		"folder": "headcheck",
 		"skills": ["Java", "Selenium WebDeiver", "Node.js", "Shell Script", "JUnit"],
-		"description": "<a href='http://headcheckhealth.com/'>HeadCheck Health Homepage</a><br><br>Due to the non-disclosure agreement with HeadCheck Health Inc., I cannot include specific details of the projects I have worked on, but here are some of the achievements I have made in the company:<br><br><ul><li>Led the development of a customer registration software which drastically reduced the redundancy in the registration process of new teams and athletes for a prospective concussion-diagnostics software startup.</li><li>Worked with the QA team to create and implement JUnit test cases for iOS, Android and web using Java and Selenium WebDriver</li><li>Communicated with the development team using Jira and Wiki.</li></ul>",
+		"description": "headcheck_health.html",
 		"thumbnail": "headcheck_1.jpg",
 		"images": ["headcheck_1.jpg"]
 	},
@@ -75,7 +75,7 @@ var projects = [{
 		"title": "Galaga on iOS",
 		"folder": "galaga",
 		"skills": ["C++", "Objective-C", "Apache Tomcat", "Java Servlet", "MySQL", "Blender", "Python"],
-		"description": 'Developed iOS clone of "Galaga", one of the most famous retro-game of all time. Used Objective-C for the UI side, and C++ for the game\'s main logic for the ease of development.<br><br>Although the code has been lost now, the original version included a login system with Tomcat Java Servlet server, and HP & Score system included with the game. The smooth motion of the enemies were created using Blender\'s Bezier curve function, and converting the baked coordinates of the curve into a data file containing 2D vectors using my custom python utility program.<br><br><iframe width="560" height="315" src="https://www.youtube.com/embed/QBm9mj1MC3Y" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>',
+		"description": 'galaga.html',
 		"thumbnail": "galaga_1.jpg",
 		"images": ["galaga_1.jpg", "galaga_2.jpg"]
 	},
@@ -84,7 +84,7 @@ var projects = [{
 		"title": "LifeTime",
 		"folder": "lifetime",
 		"skills": ["Objective-C", "Swift 3", "SQLite"],
-		"description": 'Created a time-management application for iOS. It allows users to evaluate the time spent in terms of their efficiency, and provides a well-organized graphs to allow users to see how well they spent their time.<br><br>As a student, I wanted to improve the way I use my time, and I also wanted to review how I have done over a day, a week, or a month so that I can reflect and improve based upon them. Thus, I built myself an app to do just that; increase the efficiency of time usage and receive a report on how well I did.<br><br><iframe width="560" height="315" src="https://www.youtube.com/embed/dKSFUr22j6o" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>',
+		"description": 'lifetime.html',
 		"thumbnail": "lifetime_1.jpg",
 		"images": ["lifetime_1.jpg", "lifetime_2.jpg", "lifetime_3.jpg", "lifetime_4.jpg", "lifetime_5.jpg"]
 	},
@@ -93,7 +93,7 @@ var projects = [{
 		"title": "Wizard VR Chess",
 		"folder": "wizard_chess",
 		"skills": ["C#", "Unity", "IBM Watson Speech-to-text API"],
-		"description": '',
+		"description": 'wizard_chess.html',
 		"thumbnail": "wizard_chess_1.jpg",
 		"images": ["wizard_chess_1.jpg", "wizard_chess_2.jpg", "wizard_chess_3.jpg"]
 	},
@@ -102,7 +102,7 @@ var projects = [{
 		"title": "Virtro Enterntainment",
 		"folder": "virtro",
 		"skills": ["Node.js", "Unity", "Slack API", "MySQL", "RESTful API Design"],
-		"description": '<a href="https://virtro.ca/">Virtro Enterntainment Homepage</a>',
+		"description": 'virtro.html',
 		"thumbnail": "virtro_1.jpg",
 		"images": ["virtro_1.jpg"]
 	},
@@ -179,7 +179,7 @@ $.openModal = function(projectId) {
 
 	// Description
 	$('.project-description').empty();
-	$('.project-description').append(curProject.description);
+	$('.project-description').load('/html/' + curProject.description);
 }
 
 $.closeModal = function() {
