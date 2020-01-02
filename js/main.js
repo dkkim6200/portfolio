1f526ddcc3e6f8a765b884228212564999b1d021
Daekun Kim <dkkim6200@gmail.com>
2020-01-01 16:56:19 -0800

    Changed carousel to flickity

--- js/main.js
+++ js/main.js
@@ -1,4 +1,32 @@
-var projects = [{
+var projects = [
+	{
+		"id": 10,
+		"title": "Wizard VR Chess",
+		"folder": "wizard_chess",
+		"skills": ["C#", "Unity", "IBM Watson Speech-to-text API"],
+		"description": 'wizard_chess.html',
+		"thumbnail": "wizard_chess_1.jpg",
+		"images": ["wizard_chess_1.jpg", "wizard_chess_2.jpg", "wizard_chess_3.jpg"]
+	},
+	{
+		"id": 2,
+		"title": "Reactor Engine",
+		"folder": "reactor_engine",
+		"skills": ["C++", "OpenGL", "Blender"],
+		"description": 'reactor_engine.html',
+		"thumbnail": "reactor_engine_1.jpg",
+		"images": ["reactor_engine_1.jpg", "reactor_engine_2.jpg", "reactor_engine_3.jpg"]
+	},
+	{
+		"id": 6,
+		"title": "Vector World",
+		"folder": "vector_world",
+		"skills": ["Java", "C++"],
+		"description": 'vector_world.html',
+		"thumbnail": "vector_world_1.jpg",
+		"images": ["vector_world_1.jpg", "vector_world_2.jpg"]
+	},
+	{
 		"id": 0,
 		"title": "Kindlers Network",
 		"folder": "kindlers",
@@ -17,15 +45,6 @@ var projects = [{
 		"images": ["nano_balls_1.jpg", "nano_balls_2.jpg", "nano_balls_3.jpg", "nano_balls_4.jpg", "nano_balls_5.jpg", "nano_balls_6.jpg"]
 	},
 	{
-		"id": 2,
-		"title": "Reactor Engine",
-		"folder": "reactor_engine",
-		"skills": ["C++", "OpenGL", "Blender"],
-		"description": 'reactor_engine.html',
-		"thumbnail": "reactor_engine_1.jpg",
-		"images": ["reactor_engine_1.jpg", "reactor_engine_2.jpg", "reactor_engine_3.jpg"]
-	},
-	{
 		"id": 3,
 		"title": "Spinning Coin",
 		"folder": "spinning_coin",
@@ -33,51 +52,6 @@ var projects = [{
 		"description": 'spinning_coin.html',
 		"thumbnail": "spinning_coin_1.jpg",
 		"images": ["spinning_coin_1.jpg", "spinning_coin_2.jpg"]
-	},
-	{
-		"id": 4,
-		"title": "Sympo",
-		"folder": "sympo",
-		"skills": ["Objective-C", "Swift 2", "Apache Tomcat", "Java Servlet", "MySQL"],
-		"description": "sympo.html",
-		"thumbnail": "sympo_1.jpg",
-		"images": ["sympo_1.jpg"]
-	},
-	{
-		"id": 5,
-		"title": "Tic Tac Toe",
-		"folder": "tic_tac_toe",
-		"skills": ["C++"],
-		"description": 'tic_tac_toe.html',
-		"thumbnail": "tic_tac_toe_1.jpg",
-		"images": ["tic_tac_toe_1.jpg"]
-	},
-	{
-		"id": 6,
-		"title": "Vector World",
-		"folder": "vector_world",
-		"skills": ["Java", "C++"],
-		"description": 'vector_world.html',
-		"thumbnail": "vector_world_1.jpg",
-		"images": ["vector_world_1.jpg", "vector_world_2.jpg"]
-	},
-	{
-		"id": 7,
-		"title": "HeadCheck Health<br>Automation & Testing",
-		"folder": "headcheck",
-		"skills": ["Java", "Selenium WebDeiver", "Node.js", "Shell Script", "JUnit"],
-		"description": "headcheck_health.html",
-		"thumbnail": "headcheck_1.jpg",
-		"images": ["headcheck_1.jpg"]
-	},
-	{
-		"id": 8,
-		"title": "Galaga on iOS",
-		"folder": "galaga",
-		"skills": ["C++", "Objective-C", "Apache Tomcat", "Java Servlet", "MySQL", "Blender", "Python"],
-		"description": 'galaga.html',
-		"thumbnail": "galaga_1.jpg",
-		"images": ["galaga_1.jpg", "galaga_2.jpg"]
 	},
 	{
 		"id": 9,
@@ -89,22 +63,22 @@ var projects = [{
 		"images": ["lifetime_1.jpg", "lifetime_2.jpg", "lifetime_3.jpg", "lifetime_4.jpg", "lifetime_5.jpg"]
 	},
 	{
-		"id": 10,
-		"title": "Wizard VR Chess",
-		"folder": "wizard_chess",
-		"skills": ["C#", "Unity", "IBM Watson Speech-to-text API"],
-		"description": 'wizard_chess.html',
-		"thumbnail": "wizard_chess_1.jpg",
-		"images": ["wizard_chess_1.jpg", "wizard_chess_2.jpg", "wizard_chess_3.jpg"]
+		"id": 4,
+		"title": "Sympo",
+		"folder": "sympo",
+		"skills": ["Objective-C", "Swift 2", "Apache Tomcat", "Java Servlet", "MySQL"],
+		"description": "sympo.html",
+		"thumbnail": "sympo_1.jpg",
+		"images": ["sympo_1.jpg"]
 	},
 	{
-		"id": 11,
-		"title": "Virtro Enterntainment",
-		"folder": "virtro",
-		"skills": ["Node.js", "Unity", "Slack API", "MySQL", "RESTful API Design"],
-		"description": 'virtro.html',
-		"thumbnail": "virtro_1.jpg",
-		"images": ["virtro_1.jpg"]
+		"id": 8,
+		"title": "Galaga on iOS",
+		"folder": "galaga",
+		"skills": ["C++", "Objective-C", "Apache Tomcat", "Java Servlet", "MySQL", "Blender", "Python"],
+		"description": 'galaga.html',
+		"thumbnail": "galaga_1.jpg",
+		"images": ["galaga_1.jpg", "galaga_2.jpg"]
 	},
 ]
 
@@ -112,19 +86,30 @@ var projects = [{
 var modal;
 
 // Source: https://stackoverflow.com/questions/19491336/get-url-parameter-jquery-or-how-to-get-query-string-values-in-js
-$.urlParam = function(name){
-    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-    if (results==null){
-       return null;
-    }
-    else{
-       return decodeURI(results[1]) || 0;
-    }
+$.urlParam = function (name) {
+	var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+	if (results == null) {
+		return null;
+	}
+	else {
+		return decodeURI(results[1]) || 0;
+	}
 }
 
-$.openModal = function(projectId) {
+$.openModal = function (projectId) {
 	modal.style.display = "inline-block";
-	curProject = projects[projectId];
+	curProject = undefined;
+
+	for (var i = 0; i < projects.length; i++) {
+		if (projects[i].id == projectId) {
+			curProject = projects[i];
+			break;
+		}
+	}
+
+	if (curProject == undefined) {
+		return;
+	}
 
 	// Disable scroll for the background
 	$('body').addClass('no-scroll')
@@ -138,31 +123,32 @@ $.openModal = function(projectId) {
 	$('.project-slideshow').remove();
 	$('<div></div>').addClass('project-slideshow').appendTo('.project-slideshow-placeholder');
 
-	toAppend = '';
-	for (var i = 0; i < curProject.images.length; i++) {
-		toAppend += '<img class="project-image" src="img/projects/' + curProject.folder + '/' + curProject.images[i] + '"?>';
-	}
-
-	$('.project-slideshow').append(toAppend);
-
 	// If there are multiple images, create a slideshow
 	if (curProject.images.length > 1) {
-		$('.project-slideshow').slidesjs({
-			width: $('.project-image').get(0).naturalWidth,
-			height: $('.project-image').get(0).naturalHeight,
+		toAppend = '';
+		for (var i = 0; i < curProject.images.length; i++) {
+			toAppend += '<div class="slideshow-cell"><img class="slideshow-cell-image" data-flickity-lazyload="img/projects/' + curProject.folder + '/' + curProject.images[i] + '"?></div>';
+		}
 
-	        navigation: {
-	        	active: false,
-	        	effect: "slide"
-	        },
-	        pagination: {
-	        	active: false,
-	        	effect: "slide"
-	        }
-    	});
+		$('.project-slideshow').append(toAppend);
+
+		var $carousel = $('.project-slideshow').flickity({
+			cellAlign: 'center',
+			contain: true,
+			imagesLoaded: true,
+			percentPosition: false,
+			lazyLoad: true
+		});
 	}
 	// If not, just show the picture.
 	else {
+		toAppend = '';
+		for (var i = 0; i < curProject.images.length; i++) {
+			toAppend += '<img class="slideshow-cell-image" src="img/projects/' + curProject.folder + '/' + curProject.images[i] + '"?>';
+		}
+
+		$('.project-slideshow').append(toAppend);
+
 		$('.project-slideshow').css({
 			'display': 'inline-block'
 		});
@@ -174,7 +160,7 @@ $.openModal = function(projectId) {
 	for (var i = 0; i < curProject.skills.length; i++) {
 		toAppend += curProject.skills[i] + ", ";
 	}
-	toAppend = toAppend.substring(0, toAppend.length-2);
+	toAppend = toAppend.substring(0, toAppend.length - 2);
 	$('.project-skills').append(toAppend);
 
 	// Description
@@ -182,7 +168,7 @@ $.openModal = function(projectId) {
 	$('.project-description').load('html/' + curProject.description);
 }
 
-$.closeModal = function() {
+$.closeModal = function () {
 	$('body').removeClass('no-scroll')
 	modal.style.display = "none";
 	$('.project-title').empty();
@@ -190,13 +176,13 @@ $.closeModal = function() {
 	$('.project-description').empty();
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
 	modal = document.getElementsByClassName("project-modal")[0];
 
 	for (var i = 0; i < projects.length; i++) {
 		curProject = projects[i];
 
-		toAppend = '<div class="project-cell" project="' + i + '">';
+		toAppend = '<div class="project-cell" project="' + curProject.id + '">';
 		toAppend += '<img src="img/projects/' + curProject["folder"] + '/' + curProject['thumbnail'] + '" class="thumbnail">';
 		toAppend += '<figcaption>' + curProject["title"] + '</figcaption>';
 		toAppend += '</div>'
@@ -204,7 +190,7 @@ $(document).ready(function() {
 		$('.selection-bar').append(toAppend);
 	}
 
-	$('.project-cell').click(function(e) {
+	$('.project-cell').click(function (e) {
 		e.preventDefault();
 
 		// Get the project from the project index of the clicked cell.
